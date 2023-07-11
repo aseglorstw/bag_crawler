18fe618e153d01ae730e637bb6b446c6c70e88f5
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-11 23:38:20 +0200

    new node

--- nodes/main.py
+++ nodes/main.py
@@ -1,14 +1,15 @@
 import rosbag
 import tf_reader
 import image_reader
-
+import topics_reader
 
 
 def main():
     path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/husky_2022-09-27-15-01-44.bag'
     bag = rosbag.Bag(path)
     #image_reader.save_image(bag)
-    tf_reader.create_graphs(bag)
+    #tf_reader.create_graphs(bag)
+    topics_reader.save_topics(bag)
     bag.close()
 
 
