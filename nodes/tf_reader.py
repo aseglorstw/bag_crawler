07e7baa546144cb07bdf88ce62e312c0e98a6db0
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-11 22:38:05 +0200

    I can create new graphs

--- nodes/tf_reader.py
+++ nodes/tf_reader.py
@@ -13,10 +13,11 @@ from ros_numpy import numpify
 def create_graphs(bag):
     rospy.init_node('tf_listener')
     buffer = load_buffer(bag)
-    coord_x_base_link = []
-    coord_y_base_link = []
+    coord_x_robot = []
+    coord_y_robot = []
+    coord_z_robot = []
     cloud_combined = []
-    saved_times = set()
+    saved_times = list()
     for topic, msg, time in bag.read_messages(topics=['/points']):
         time = rospy.Time.from_sec(time.to_sec())
         time_sec = time.to_sec()
@@ -27,23 +28,27 @@ def create_graphs(bag):
             msg = PointCloud2(*slots(msg))
             cloud = np.array(list(read_points(msg)))
             try:
+                transform_map_base_link = buffer.lookup_transform_full("map", time, "base_link", time, "map",
+                                                                       rospy.Duration(1))
+                coord_x_robot.append(transform_map_base_link.transform.translation.x)
+                coord_y_robot.append(transform_map_base_link.transform.translation.y)
+                coord_z_robot.append(transform_map_base_link.transform.translation.z)
+                saved_times.append(save_time)
                 transform_map_os_sensor = buffer.lookup_transform_full("map", time, "os_sensor", time, "map",
                                                                        rospy.Duration(1))
                 matrix = numpify(transform_map_os_sensor.transform)
                 vectors = np.array([cloud[::200, 0], cloud[::200, 1], cloud[::200, 2]])
                 transformed_vectors = matrix[:3, :3] @ vectors + matrix[:3, 3:4]
                 cloud_combined.append(transformed_vectors)
-                transform_map_base_link = buffer.lookup_transform_full("map", time, "base_link", time, "map",
-                                                                       rospy.Duration(1))
-                coord_x_base_link.append(transform_map_base_link.transform.translation.x)
-                coord_y_base_link.append(transform_map_base_link.transform.translation.y)
-                saved_times.add(save_time)
             except ExtrapolationException:
                 continue
     if len(cloud_combined) > 0:
-        save_png(cloud_combined, coord_x_base_link, coord_y_base_link)
-        
+        create_graph_tf_and_point_cloud(cloud_combined, coord_x_robot, coord_y_robot)
+        create_graph_x_coord_and_time(coord_x_robot, saved_times)
+        create_graph_y_coord_and_time(coord_y_robot, saved_times)
+        create_graph_z_coord_and_time(coord_z_robot, saved_times)
 
+        
 def slots(msg):
     return [getattr(msg, var) for var in msg.__slots__]
 
@@ -65,7 +70,7 @@ def load_buffer(bag):
     return buffer
 
 
-def save_png(cloud_combined, coord_x_base_link, coord_y_base_link):
+def create_graph_tf_and_point_cloud(cloud_combined, coord_x_robot, coord_y_robot):
     output_path = "/home/robert/catkin_ws/src/bag_crawler/nodes/web_server/shared_point_cloud.png"
     fig, ax = plt.subplots()
     marker_size = 0.5
@@ -76,8 +81,40 @@ def save_png(cloud_combined, coord_x_base_link, coord_y_base_link):
     colors = combined_points[2, :]
     colors += abs(np.min(colors))
     transformed_colors = np.log1p(colors)
-    print(np.min(transformed_colors), np.max(transformed_colors))
-    ax.scatter(combined_points[0, :], combined_points[1, :], s=marker_size, c=transformed_colors , cmap='Blues')
-    ax.plot(coord_x_base_link, coord_y_base_link, color='red')
+    ax.scatter(combined_points[0, :], combined_points[1, :], s=marker_size, c=transformed_colors, cmap='Blues')
+    ax.plot(coord_x_robot, coord_y_robot, color='red')
+    plt.savefig(output_path)
+    plt.close()
+
+
+def create_graph_x_coord_and_time(coord_x_robot, saved_times):
+    output_path = "/home/robert/catkin_ws/src/bag_crawler/nodes/web_server/coord_x_and_time.png"
+    fig, ax = plt.subplots()
+    plt.xlabel('Time')
+    plt.ylabel('X-coordinate')
+    plt.title("Change of coordinate X with time")
+    ax.plot(saved_times, coord_x_robot, color='blue')
+    plt.savefig(output_path)
+    plt.close()
+
+
+def create_graph_y_coord_and_time(coord_y_robot, saved_times):
+    output_path = "/home/robert/catkin_ws/src/bag_crawler/nodes/web_server/coord_y_and_time.png"
+    fig, ax = plt.subplots()
+    plt.xlabel('Time')
+    plt.ylabel('Y-coordinate')
+    plt.title("Change of coordinate Y with time")
+    ax.plot(saved_times, coord_y_robot, color='blue')
+    plt.savefig(output_path)
+    plt.close()
+
+
+def create_graph_z_coord_and_time(coord_z_robot, saved_times):
+    output_path = "/home/robert/catkin_ws/src/bag_crawler/nodes/web_server/coord_z_and_time.png"
+    fig, ax = plt.subplots()
+    plt.xlabel('Time')
+    plt.ylabel('Z-coordinate')
+    plt.title("Change of coordinate Z with time")
+    ax.plot(saved_times, coord_z_robot, color='blue')
     plt.savefig(output_path)
     plt.close()
