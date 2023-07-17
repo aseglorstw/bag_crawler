1bc511445539bfcb863dface8e28448987f8f470
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-17 16:02:13 +0200

    new commit

--- nodes/image_reader.py
+++ nodes/image_reader.py
@@ -3,10 +3,10 @@ from sensor_msgs.msg import CompressedImage
 import cv2
 
 
-def save_image(bag):
+def save_video(bag):
     output_path = "/home/robert/catkin_ws/src/bag_crawler/nodes/web_server/video.avi"
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    video_out = cv2.VideoWriter(output_path, fourcc, 30.0, (1920, 1200), True)
+    video_out = cv2.VideoWriter(output_path, fourcc, 20.0, (1920, 1200), True)
     for topic, msg, time in bag.read_messages(topics=['/camera_front/image_color/compressed']):
         msg = CompressedImage(*slots(msg))
         np_arr = np.fromstring(msg.data, np.uint8)
--- nodes/main.py
+++ nodes/main.py
@@ -8,7 +8,7 @@ import bag_info_reader
 def main():
     path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/husky_2022-09-27-15-01-44.bag'
     bag = rosbag.Bag(path)
-    image_reader.save_image(bag)
+    image_reader.save_video(bag)
     #tf_reader.create_graphs(bag)
     #topics_reader.save_topics_info(bag)
     #topics_reader.create_graph_control_joy_and_time(bag)
