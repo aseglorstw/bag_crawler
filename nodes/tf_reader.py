ffa69cf848ae720ffc72decd94a8b6c664a4cb54
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-18 13:57:04 +0200

    new commit

--- nodes/tf_reader.py
+++ nodes/tf_reader.py
@@ -34,9 +34,9 @@ def create_graphs(bag):
             if msg_number % 25 == 0:
                 msg = PointCloud2(*slots(msg))
                 cloud = np.array(list(read_points(msg)))
-                transform_map_os_sensor = buffer.lookup_transform_full("map", time, msg.header.frame_id, time, "map",
-                                                                       rospy.Duration(1))
-                matrix = numpify(transform_map_os_sensor.transform)
+                transform_map_lidar = buffer.lookup_transform_full("map", time, msg.header.frame_id, time,
+                                                                   "map", rospy.Duration(1))
+                matrix = numpify(transform_map_lidar.transform)
                 vectors = np.array([cloud[::200, 0], cloud[::200, 1], cloud[::200, 2]])
                 transformed_vectors = matrix[:3, :3] @ vectors + matrix[:3, 3:4]
                 cloud_combined.append(transformed_vectors)
