4480a98c688157ece9c64b2a8bc797d4b7f4f55c
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-18 18:14:42 +0200

    new

--- nodes/main.py
+++ nodes/main.py
@@ -1,14 +1,15 @@
 import rosbag
+import writer_to_files
 import topics_reader
-import image_reader
-import info_topics_reader
-import bag_info_reader
 
 
 def main():
     path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/husky_2022-09-27-15-01-44.bag'
     bag = rosbag.Bag(path)
-    topics_reader.read_lidar_topic_and_icp_with_odom(bag)
+    # topics_reader.read_lidar_topic_and_icp_with_odom(bag)
+    # topics_reader.read_image_topic(bag)
+    # topics_reader.read_joy_topic_and_icp(bag)
+    writer_to_files.write_topics_info(bag)
     bag.close()
 
 
--- nodes/writer_to_files.py
+++ nodes/writer_to_files.py
@@ -8,7 +8,7 @@ def write_bag_info(bag, distances_icp,  start_of_moving, end_of_moving, speeds):
     info_dict = yaml.load(bag._get_yaml_info(), Loader=yaml.Loader)
     speeds = np.array(speeds)
     average_speed_icp = np.sum(speeds) / len(speeds)
-    with open(output_path, "r+", encoding="utf-8") as file:
+    with open(output_path, "w", encoding="utf-8") as file:
         file.seek(0, 2)
         file.write(f"{distances_icp[-1]}\n{average_speed_icp}\n{start_of_moving}\n{end_of_moving}\n"
                 f"{utils.get_date(info_dict['start'])}\n{utils.get_date(info_dict['end'])}\n{info_dict['duration']}\n"
