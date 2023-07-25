d0c963d392a06156b2089edd41e436fbffefebf1
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-07-25 12:14:23 +0200

    nw co

--- nodes/calculator.py
+++ nodes/calculator.py
@@ -3,72 +3,66 @@ from math import sqrt
 from ros_numpy import numpify
 
 
-class Calculater:
-    def __init__(self, icp, odom, saved_times):
-        self.icp = np.array(icp)
-        self.odom = np.array(odom)
-        self.distance = 0
-        self.speeds = []
-        self.saved_times = saved_times
+def get_distances(coordinates):
+    coordinates = np.array(coordinates)
+    transpose_coordinates = coordinates.T
+    distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
+    distances_xyz = np.concatenate((np.zeros((1, 3)), np.cumsum(distances_one_period, axis=0)), axis=0)
+    distances = np.sqrt(np.sum(np.power(distances_xyz, 2), axis=1))
+    return distances.tolist()
 
-    def get_distances(self, coord):
-        coord = np.array(coord)
-        distances_one_period = np.abs(coord[1:] - coord[:-1])
-        distances_xyz = [[0, 0, 0]]
-        distances = [0]
-        for distance in distances_one_period:
-            distances_xyz.append(distances_xyz[-1] + distance)
-            distances.append(sqrt(pow(distances_xyz[-1][0], 2) + pow(distances_xyz[-1][1], 2) +
-                                  pow(distances_xyz[-1][2], 2)))
-        self.distance = distances[-1]
-        return distances
 
-    def get_start_and_end_of_moving(self):
-        start_of_moving = -1
-        end_of_moving = -1
-        for i in range(len(self.speeds)):
-            if self.speeds[i] > 0.2 and start_of_moving == -1:
-                start_of_moving = self.saved_times[i]
-            elif self.speeds[i] < 0.2 or (i == len(self.speeds) - 1 and self.speeds[i] > 0.2):
-                end_of_moving = self.saved_times[i]
-        return start_of_moving, end_of_moving
+def get_start_and_end_of_moving(speeds, saved_times):
+    start_of_moving = -1
+    end_of_moving = -1
+    for i in range(len(speeds)):
+        if speeds[i] > 0.2 and start_of_moving == -1:
+            start_of_moving = saved_times[i]
+        elif speeds[i] < 0.2 or (i == len(speeds) - 1 and speeds[i] > 0.2):
+            end_of_moving = saved_times[i]
+    return start_of_moving, end_of_moving
 
-    def get_speeds_one_period(self):
-        distances_one_period = np.abs(self.icp[1:] - self.icp[:-1])
-        saved_times = np.array(self.saved_times)
-        times_one_period = saved_times[1:] - saved_times[:-1]
-        speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
-        for speed in speeds_xyz:
-            self.speeds.append(sqrt(pow(speed[0], 2) + pow(speed[1], 2) + pow(speed[2], 2)))
-        return self.speeds
 
-    def get_joy_control_coordinates(self, joy_control_times):
-        indices = np.unique(np.searchsorted(self.saved_times, joy_control_times))
-        split_indices = np.concatenate(([-1], np.where(np.diff(indices) > 1)[0], [len(indices) - 1]))
-        split_indices = [indices[split_indices[i] + 1:split_indices[i + 1] + 1] for i in range(len(split_indices) - 1)]
-        control_coordinates = []
-        for indices in split_indices:
-            control_coordinates.append((self.icp - self.icp[0])[indices[indices < len(self.saved_times)]])
-        return control_coordinates
+def get_speeds_one_period(coordinates, saved_times):
+    speeds = []
+    coordinates = np.array(coordinates)
+    transpose_coordinates = coordinates.T
+    distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
+    saved_times = np.array(saved_times)
+    times_one_period = saved_times[1:] - saved_times[:-1]
+    speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
+    for speed in speeds_xyz:
+        speeds.append(sqrt(pow(speed[0], 2) + pow(speed[1], 2) + pow(speed[2], 2)))
+    return speeds
 
-    @staticmethod
-    def get_joy_control_binary(self, joy_control_times):
-        indices = np.unique(np.searchsorted(self.saved_times, joy_control_times))
-        control_binary = np.zeros(len(self.saved_times), dtype=int)
-        control_binary[indices[indices < len(self.saved_times)]] = 1
-        return control_binary
 
-    @staticmethod
-    def transform_trajectory(coordinates, matrix):
-        inv_matrix = np.linalg.inv(numpify(matrix)[:3, :3])
-        coordinates = np.concatenate(coordinates, axis=1)
-        transformed_coordinates = inv_matrix @ coordinates - np.expand_dims(inv_matrix @ coordinates[:, 0], axis=1)
-        return transformed_coordinates
+def get_joy_control_coordinates(coordinates, joy_control_times, saved_times):
+    indices = np.unique(np.searchsorted(saved_times, joy_control_times))
+    split_indices = np.concatenate(([-1], np.where(np.diff(indices) > 1)[0], [len(indices) - 1]))
+    split_indices = [indices[split_indices[i] + 1:split_indices[i + 1] + 1] for i in range(len(split_indices) - 1)]
+    control_coordinates = []
+    for indices in split_indices:
+        control_coordinates.append((coordinates - coordinates[0])[indices[indices < len(saved_times)]])
+    return control_coordinates
 
-    @staticmethod
-    def transform_point_cloud(point_cloud, matrix):
-        inv_matrix = np.linalg.inv(numpify(matrix)[:3, :3])
-        point_cloud = np.concatenate(point_cloud, axis=1)
-        first_transform = np.array([[matrix.translation.x], [matrix.translation.y], [matrix.translation.z]])
-        transformed_point_cloud = inv_matrix @ point_cloud - inv_matrix @ first_transform
-        return transformed_point_cloud
+
+def get_joy_control_binary(saved_times, joy_control_times):
+    indices = np.unique(np.searchsorted(saved_times, joy_control_times))
+    control_binary = np.zeros(len(saved_times), dtype=int)
+    control_binary[indices[indices < len(saved_times)]] = 1
+    return control_binary
+
+
+def transform_trajectory(coordinates, matrix):
+    inv_matrix = np.linalg.inv(numpify(matrix)[:3, :3])
+    coordinates = np.concatenate(coordinates, axis=1)
+    transformed_coordinates = inv_matrix @ coordinates - np.expand_dims(inv_matrix @ coordinates[:, 0], axis=1)
+    return transformed_coordinates
+
+
+def transform_point_cloud(point_cloud, matrix):
+    inv_matrix = np.linalg.inv(numpify(matrix)[:3, :3])
+    point_cloud = np.concatenate(point_cloud, axis=1)
+    first_transform = np.array([[matrix.translation.x], [matrix.translation.y], [matrix.translation.z]])
+    transformed_point_cloud = inv_matrix @ point_cloud - inv_matrix @ first_transform
+    return transformed_point_cloud
--- nodes/main.py
+++ nodes/main.py
@@ -3,44 +3,48 @@ import topics_reader
 import graphs_creator
 import calculator
 import writer_to_files
+import os
 
 
 def main():
-    path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/husky_2022-09-27-15-01-44.bag'
-    #path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/husky_2022-09-23-12-38-31.bag'
-    bag = rosbag.Bag(path)
+    path = '/home/robert/catkin_ws/src/bag_crawler/bagfiles/'
+    bag_file = 'husky_2022-09-27-15-01-44.bag'
+    folder = path + ".web_server_" + bag_file
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    bag = rosbag.Bag(path + bag_file)
 
     reader = topics_reader.Reader(bag)
-    #reader.load_buffer()
+    reader.load_buffer()
     #point_cloud = reader.read_point_cloud()
-    #icp, odom, saved_times = reader.read_icp_odom()
-    #first_matrix_icp, first_matrix_odom = reader.get_first_rotation_matrices()
+    icp, odom, saved_times = reader.read_icp_odom()
+    first_matrix_icp, first_matrix_odom = reader.get_first_rotation_matrices()
     #reader.read_images_and_save_video()
-    #joy_control_times = reader.read_joy_topic()
+    # joy_control_times = reader.read_joy_topic()
 
-    #calc = calculator.Calculater(icp, odom, saved_times)
-    # distances_icp = calc.get_distances(icp)
-    # distances_odom = calc.get_distances(odom)
-    # speeds = calc.get_speeds_one_period()
-    # start_of_moving, end_of_moving = calc.get_start_and_end_of_moving()
-    # joy_control_coordinates = calc.get_joy_control_coordinates(joy_control_times)
-    # joy_control_binary = calc.get_joy_control_binary(joy_control_times)
-    #transformed_icp = calc.transform_trajectory(icp, first_matrix_icp)
-    #transformed_odom = calc.transform_trajectory(odom, first_matrix_odom)
-    #transformed_point_cloud = calc.transform_point_cloud(point_cloud, first_matrix_icp)
+    transformed_icp = calculator.transform_trajectory(icp, first_matrix_icp)
+    transformed_odom = calculator.transform_trajectory(odom, first_matrix_odom)
+    #transformed_point_cloud = calculator.transform_point_cloud(point_cloud, first_matrix_icp)
+    distances_icp = calculator.get_distances(transformed_icp)
+    distances_odom = calculator.get_distances(transformed_odom)
+    speeds = calculator.get_speeds_one_period(transformed_icp, saved_times)
+    start_of_moving, end_of_moving = calculator.get_start_and_end_of_moving(speeds, saved_times)
+    # joy_control_coordinates = calculator.get_joy_control_coordinates(joy_control_times)
+    # joy_control_binary = calculator.get_joy_control_binary(joy_control_times)
 
-    #creator = graphs_creator.GraphsCreator(transformed_icp, transformed_odom, saved_times)
+
+    # creator = graphs_creator.GraphsCreator(transformed_icp, transformed_odom, saved_times)
     # creator.create_graph_x_over_time()
     # creator.create_graph_y_over_time()
     # creator.create_graph_z_over_time()
-    #creator.create_graph_xy_and_point_cloud(transformed_point_cloud)
-    #creator.show_point_cloud(transformed_point_cloud)
+    # creator.create_graph_xy_and_point_cloud(transformed_point_cloud)
+    # creator.show_point_cloud(transformed_point_cloud)
     # creator.create_graph_distance_over_time(distances_icp, distances_odom, start_of_moving, end_of_moving)
     # creator.create_graph_joy_control_times_and_icp(joy_control_coordinates)
     # creator.create_binary_graph_joy_control_and_time(joy_control_binary)
 
-    writer = writer_to_files.Writer(bag)
-    writer.write_topics_info()
+    # writer = writer_to_files.Writer(bag)
+    # writer.write_topics_info()
     # writer.write_bag_info()
 
     bag.close()
