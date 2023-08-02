ed44a378f72a99d061fda9fbaac70fef280d61ba
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-08-02 15:24:09 +0200

    change name

--- nodes/directory_scanner.py
+++ nodes/directory_scanner.py
@@ -17,10 +17,10 @@ class Scanner:
         return web_folder
     
     def create_task_list(self):
-        task_list = dict()
         bag_files = list(self.find_bag_files())
         for bag_file in bag_files:
-            task_list[bag_file] = self.find_web_folder(bag_file)
+            if not self.find_web_folder(bag_file):
+                yield bag_file
 
     def find_loc_file(self, bag_file_name):
         for file in pathlib.Path(self.directory).iterdir():
@@ -29,16 +29,11 @@ class Scanner:
         return None
 
     def find_bag_files(self):
-        stop_suffixes = ["full_tf", "_loc", "orig", "params", "no_sensors", "web_server"]
+        stop_suffixes = ["_loc", "params", "no_sensors", "web_server"]
         for file in pathlib.Path(self.directory).iterdir():
             if file.suffix == ".bag" and not any(suffix in file.stem for suffix in stop_suffixes):
-                print(file.name)
                 yield file.name
 
     def find_web_folder(self, bag_file):
         web_folder = os.path.join(self.directory, f".web_server_{bag_file}")
-        if self.path_check(web_folder):
-            return None
-        for file in pathlib.Path(web_folder).iterdir():
-            print(file.name)
-        return "a"
+        return self.path_check(web_folder)
