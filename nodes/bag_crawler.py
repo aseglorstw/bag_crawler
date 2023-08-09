c4c45982833f7f5e919bb3e564d6944b30c2eafc
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-08-09 14:15:52 +0200

    scanner of directory

--- nodes/bag_crawler.py
+++ nodes/bag_crawler.py
@@ -11,54 +11,56 @@ import schedule
 
 
 def main(directory):
-    directory_scanner = DirectoryScanner(directory)
+    directory_scanner = DirectoryScanner()
     if not directory_scanner.input_check(directory):
         return 1
-    task_list = list(directory_scanner.create_task_list())
+    task_list = directory_scanner.create_task_list(root)
     for bag_file in task_list:
-        bag = open_bag_file(directory, bag_file)
+        bag = open_bag_file(bag_file)
         if bag is None:
             continue
         output_folder = directory_scanner.create_output_folder(bag_file)
         loc_file_name = directory_scanner.find_loc_file(bag_file)
-        loc_file = rosbag.Bag(os.path.join(directory, loc_file_name)) if loc_file_name is not None else None
-        reader = Reader(bag, loc_file)
-        icp, odom, saved_times, first_matrix_icp, first_matrix_odom = reader.read_icp_odom()
-        point_cloud = list(reader.read_point_cloud())
-        joy_control_times = reader.read_joy_topic()
-        reader.read_images_and_save_video(output_folder)
+        print(loc_file_name)
+        #loc_file = rosbag.Bag(os.path.join(directory, loc_file_name)) if loc_file_name is not None else None
 
-        transformed_icp = calculator.transform_trajectory(icp, first_matrix_icp)
-        transformed_odom = calculator.transform_trajectory(odom, first_matrix_odom)
-        transformed_point_cloud = calculator.transform_point_cloud(point_cloud, first_matrix_icp)
-        distances_icp = calculator.get_distances(transformed_icp)
-        distances_odom = calculator.get_distances(transformed_odom)
-        speeds = calculator.get_speeds_one_period(transformed_icp, transformed_odom, saved_times)
-        average_speed = calculator.get_average_speed(speeds)
-        start_of_moving, end_of_moving = calculator.get_start_and_end_of_moving(speeds, saved_times)
-        joy_control_coordinates = calculator.get_joy_control_coordinates(transformed_icp, joy_control_times,
-                                                                         saved_times)
-        graphs_creator.create_graph_x_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
-        graphs_creator.create_graph_y_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
-        graphs_creator.create_graph_z_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
-        graphs_creator.create_graph_distance_over_time(distances_icp, distances_odom, saved_times, start_of_moving,
-                                                       end_of_moving, output_folder)
-        graphs_creator.create_graph_xy_and_point_cloud(transformed_odom, transformed_icp, transformed_point_cloud,
-                                                       output_folder)
-        graphs_creator.create_graph_joy_control_times_and_icp(transformed_icp, joy_control_coordinates,
-                                                              output_folder)
-        writer = Writer(bag, output_folder)
-        writer.write_topics_info()
-        writer.write_bag_info(distances_icp[-1], start_of_moving, end_of_moving, average_speed)
-        close_bag_file(bag, bag_file)
+        # reader = Reader(bag, loc_file)
+        # icp, odom, saved_times, first_matrix_icp, first_matrix_odom = reader.read_icp_odom()
+        # point_cloud = list(reader.read_point_cloud())
+        # joy_control_times = reader.read_joy_topic()
+        # reader.read_images_and_save_video(output_folder)
+        #
+        # transformed_icp = calculator.transform_trajectory(icp, first_matrix_icp)
+        # transformed_odom = calculator.transform_trajectory(odom, first_matrix_odom)
+        # transformed_point_cloud = calculator.transform_point_cloud(point_cloud, first_matrix_icp)
+        # distances_icp = calculator.get_distances(transformed_icp)
+        # distances_odom = calculator.get_distances(transformed_odom)
+        # speeds = calculator.get_speeds_one_period(transformed_icp, transformed_odom, saved_times)
+        # average_speed = calculator.get_average_speed(speeds)
+        # start_of_moving, end_of_moving = calculator.get_start_and_end_of_moving(speeds, saved_times)
+        # joy_control_coordinates = calculator.get_joy_control_coordinates(transformed_icp, joy_control_times,
+        #                                                                  saved_times)
+        # graphs_creator.create_graph_x_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
+        # graphs_creator.create_graph_y_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
+        # graphs_creator.create_graph_z_over_time(transformed_odom, transformed_icp, saved_times, output_folder)
+        # graphs_creator.create_graph_distance_over_time(distances_icp, distances_odom, saved_times, start_of_moving,
+        #                                                end_of_moving, output_folder)
+        # graphs_creator.create_graph_xy_and_point_cloud(transformed_odom, transformed_icp, transformed_point_cloud,
+        #                                                output_folder)
+        # graphs_creator.create_graph_joy_control_times_and_icp(transformed_icp, joy_control_coordinates,
+        #                                                       output_folder)
+        # writer = Writer(bag, output_folder)
+        # writer.write_topics_info()
+        # writer.write_bag_info(distances_icp[-1], start_of_moving, end_of_moving, average_speed)
+        # close_bag_file(bag, bag_file)
 
 
-def open_bag_file(directory, bag_file):
+def open_bag_file(bag_file):
     try:
-        bag = rosbag.Bag(os.path.join(directory, bag_file))
+        bag = rosbag.Bag(bag_file)
         return bag
-    except rosbag.ROSBagException as e:
-        print(f"When opening the file {bag_file}, a {e} error occurred")
+    except Exception as e:
+        print(f"When opening the file {bag_file}, a '{e}' error occurred")
         return None
 
 
@@ -71,8 +73,8 @@ def close_bag_file(bag, bag_file):
 
 if __name__ == '__main__':
     root = sys.argv[1]
-    # execution_time = timeit.timeit(lambda: main(arg1), number=1)
-    # print(f"Время выполнения программы: {execution_time:.6f} секунд")
-    schedule.every(10).seconds.do(lambda: main(root))
-    while True:
-        schedule.run_pending()
+    execution_time = timeit.timeit(lambda: main(root), number=1)
+    print(f"Время выполнения программы: {execution_time:.6f} секунд")
+    # schedule.every(10).seconds.do(lambda: main(root))
+    # while True:
+    #     schedule.run_pending()
--- nodes/directory_scanner.py
+++ nodes/directory_scanner.py
@@ -1,43 +1,53 @@
 import pathlib
 import os
-import sys
+from logger import setup_logger
 
+logger = setup_logger()
 
 class DirectoryScanner:
-    def __init__(self, directory):
-        self.directory = directory
+    def __init__(self):
+        self.task_list = []
+
+    def create_task_list(self, root):
+        self.find_bag_files(root)
+        return self.task_list
+
+    def find_bag_files(self, directory):
+        stop_suffixes = ["_loc", "params", "no_sensors"]
+        items = os.listdir(directory)
+        folders = [os.path.join(directory, item) for item in items if
+                   os.path.isdir(os.path.join(directory, item)) and ".web_server" not in item]
+        for folder in folders:
+            self.find_bag_files(folder)
+        for file in pathlib.Path(directory).iterdir():
+            if file.is_file() and file.suffix == ".bag" and not any(suffix in file.stem for suffix in stop_suffixes):
+                if not self.find_web_folder(directory, file.name):
+                    self.task_list.append(os.path.join(directory, file.name))
 
     @staticmethod
     def input_check(path):
         if not (os.path.exists(path) and os.path.isdir(path)):
-            print("This directory doesn't exist.")
+            logger.error("This directory doesn't exist.")
             return False
         return True
 
-    def create_output_folder(self, bag_file):
-        web_folder = os.path.join(self.directory, f".web_server_{bag_file}")
+    @staticmethod
+    def create_output_folder(bag_file):
+        directory, file_name = bag_file.rsplit('/', 1)
+        web_folder = os.path.join(directory, f".web_server_{file_name}")
         if not os.path.exists(web_folder):
             os.mkdir(web_folder)
         return web_folder
-    
-    def create_task_list(self):
-        bag_files = list(self.find_bag_files())
-        for bag_file in bag_files:
-            if not self.find_web_folder(bag_file):
-                yield bag_file
 
-    def find_loc_file(self, bag_file_name):
-        for file in pathlib.Path(self.directory).iterdir():
-            if bag_file_name.replace(".bag", "_loc.bag") in file.name:
+    @staticmethod
+    def find_loc_file(bag_file_name):
+        directory, file_name = bag_file_name.rsplit('/', 1)
+        for file in pathlib.Path(directory).iterdir():
+            if file_name.replace(".bag", "_loc.bag") in file.name:
                 return file.name
         return None
 
-    def find_bag_files(self):
-        stop_suffixes = ["_loc", "params", "no_sensors", "web_server"]
-        for file in pathlib.Path(self.directory).iterdir():
-            if file.suffix == ".bag" and not any(suffix in file.stem for suffix in stop_suffixes):
-                yield file.name
-
-    def find_web_folder(self, bag_file):
-        web_folder = os.path.join(self.directory, f".web_server_{bag_file}")
+    @staticmethod
+    def find_web_folder(directory, bag_file):
+        web_folder = os.path.join(directory, f".web_server_{bag_file}")
         return os.path.exists(web_folder) and os.path.isdir(web_folder)
