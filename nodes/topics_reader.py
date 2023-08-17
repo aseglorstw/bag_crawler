c391bbf7ff75d89ef8ccd27e5c05c66059cee7b2
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-08-17 13:39:18 +0200

    If icp, odom, point_cloud are proccesed, I load these data from disk

--- nodes/topics_reader.py
+++ nodes/topics_reader.py
@@ -18,23 +18,25 @@ class Reader:
 
     def __init__(self, bag, loc_file):
         self.bags = [bag, loc_file] if loc_file is not None else [bag]
+        rospy.init_node('tf_listener')
         self.buffer = None
+        self.load_buffer()
         self.start_time = bag.get_start_time()
         self.topics_info = self.bags[0].get_type_and_topic_info()[1]
-        rospy.init_node('tf_listener')
-        self.load_buffer()
-        self.data_availability = {"icp": True, "odom": True, "point_cloud": True,  "video": True}
-        self.matrices_icp = []
+        self.matrices_base_link_map = []
         self.saved_times_icp = []
+        self.matrices_base_link_odom = []
+        self.saved_times_odom = []
+        self.data_availability = {"icp": True, "odom": True, "point_cloud": True, "video": True}
 
     def read_point_cloud(self):
         topic_name = self.find_points_topic()
         if topic_name is None:
-            print("The topic lidar posted to was not found")
             self.data_availability["point_cloud"] = False
+            print("The topic lidar posted to was not found")
             return None
         save_interval = 20
-        matrix_from_base_link_to_lidar = self.get_matrix_from_lidar_to_base_link(topic_name)
+        matrix_lidar_base_link = self.get_matrix_from_lidar_to_base_link(topic_name)
         for msg_number, (topic, msg, time) in enumerate(self.bags[0].read_messages(topics=[topic_name])):
             if msg_number % save_interval == 0:
                 time = rospy.Time.from_sec(time.to_sec())
@@ -42,63 +44,67 @@ class Reader:
                 msg = PointCloud2(*self.slots(msg))
                 cloud = np.array(list(read_points(msg)))
                 vectors = np.array([cloud[::200, 0], cloud[::200, 1], cloud[::200, 2]])
-                matrix = self.get_matrix_from_lidar_to_static_frame(matrix_from_base_link_to_lidar, save_time)
-                transformed_vectors = matrix[:3, :3] @ vectors + matrix[:3, 3:4]
+                matrix_lidar_static_frame = self.get_matrix_from_lidar_to_static_frame(matrix_lidar_base_link, save_time)
+                transformed_vectors = matrix_lidar_static_frame[:3, :3] @ vectors + matrix_lidar_static_frame[:3, 3:4]
                 print(f"Point cloud is saved. Time: {save_time}")
                 yield transformed_vectors
 
     def read_odom(self):
         odom = []
-        saved_times_odom = []
-        rotation_matrix_odom = None
+        first_rotation_matrix_odom = None
         topic_name = self.find_odom_topic()
         if topic_name is None:
             print("The topic imu_odom was not found")
             self.data_availability["odom"] = False
-            return np.array(odom), np.array(saved_times_odom), rotation_matrix_odom
+            return np.array(odom), np.array(self.saved_times_odom), first_rotation_matrix_odom
         for topic, msg, time in self.bags[0].read_messages(topics=[topic_name]):
             time = rospy.Time.from_sec(time.to_sec())
             save_time = time.to_sec() - self.start_time
             position = msg.pose.pose.position
             orientation = msg.pose.pose.orientation
+            quaternion = Quaternion(orientation.w, orientation.x, orientation.y, orientation.z)
+            self.add_matrix_to_matrices_base_link_static_frame(quaternion.rotation_matrix,
+                                                               [position.x, position.y, position.z],
+                                                               self.matrices_base_link_odom)
             odom.append(np.array([[position.x], [position.y], [position.z]]))
-            if rotation_matrix_odom is None:
+            if first_rotation_matrix_odom is None:
                 quaternion = Quaternion(orientation.w, orientation.x, orientation.y, orientation.z)
-                rotation_matrix_odom = quaternion.rotation_matrix
+                first_rotation_matrix_odom = quaternion.rotation_matrix
             print(f"The Coordinates from frame 'base_link' to frame 'odom' are saved. Time: {save_time}")
-            saved_times_odom.append(save_time)
-        return np.array(odom), np.array(saved_times_odom), rotation_matrix_odom
+            self.saved_times_odom.append(save_time)
+        return np.array(odom), np.array(self.saved_times_odom), first_rotation_matrix_odom
 
     def read_icp(self):
         icp = []
-        saved_times_icp = []
         first_rotation_matrix_icp = None
         first_transform_icp = None
         topic_name = self.find_icp_topic()
         if topic_name is None:
             print("The topic icp_odom was not found")
             self.data_availability["icp"] = False
-            return np.array(icp), np.array(saved_times_icp), first_rotation_matrix_icp
+            return np.array(icp), np.array(self.saved_times_icp), first_rotation_matrix_icp
         for topic, msg, time in self.bags[0].read_messages(topics=[topic_name]):
             save_time = rospy.Time.from_sec(time.to_sec()).to_sec() - self.start_time
             position = msg.pose.pose.position
             orientation = msg.pose.pose.orientation
             quaternion = Quaternion(orientation.w, orientation.x, orientation.y, orientation.z)
-            self.add_matrix_to_matrices_icp(quaternion, position)
+            self.add_matrix_to_matrices_base_link_static_frame(quaternion.rotation_matrix,
+                                                               [position.x, position.y, position.z],
+                                                               self.matrices_base_link_map)
             icp.append(np.array([[position.x], [position.y], [position.z]]))
             if first_rotation_matrix_icp is None:
                 first_rotation_matrix_icp = quaternion.rotation_matrix
                 first_transform_icp = np.array([[position.x], [position.y], [position.z]])
             print(f"The Coordinates from frame 'base_link' to frame 'map' are saved. Time: {save_time}")
-            saved_times_icp.append(save_time)
-        self.saved_times_icp = np.array(saved_times_icp)
-        return np.array(icp), np.array(saved_times_icp), first_rotation_matrix_icp, first_transform_icp
+            self.saved_times_icp.append(save_time)
+        return np.array(icp), np.array(self.saved_times_icp), first_rotation_matrix_icp, first_transform_icp
 
     def read_images_and_save_video(self, folder):
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
         save_interval = 5
         topic_names = list(self.find_camera_topic())
         if topic_names[0] is None:
+            self.data_availability["video"] = False
             print("The topic in which messages from the camera are posted was not found")
             return None
         for topic_name in topic_names:
@@ -115,22 +121,9 @@ class Reader:
                     current_datetime = self.get_datetime(time_from_start)
                     cv2.putText(image, current_datetime, (30, 90), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
                     video_out.write(image)
-                    self.data_availability["video"] = True
                     print(f"Image from topic {topic_name} for video is saved. Time: {time.to_sec() - self.start_time}")
             print(f"Video  from topic {topic_name} is saved.")
             video_out.release()
-
-    def add_matrix_to_matrices_icp(self, quaternion, position):
-        transform_matrix = np.eye(4)
-        transform_matrix[:3, :3] = quaternion.rotation_matrix
-        transform_matrix[0, 3] = position.x
-        transform_matrix[1, 3] = position.y
-        transform_matrix[2, 3] = position.z
-        self.matrices_icp.append(transform_matrix)
-
-    def get_matrix_from_lidar_to_static_frame(self, matrix_base_link_lidar, save_time):
-        matrix_icp = self.matrices_icp[np.unique(np.searchsorted(self.saved_times_icp, save_time))[0]]
-        return matrix_icp @ matrix_base_link_lidar
 
     def read_joy_topic(self):
         joy_control_times = []
@@ -150,8 +143,7 @@ class Reader:
                 for topic, msg, time in tqdm(bag.read_messages(topics='/tf_static'),
                                              total=bag.get_message_count(topic_filters='/tf_static')):
                     for tf in msg.transforms:
-                        if topic == '/tf_static':
-                            self.buffer.set_transform_static(tf, 'bag')
+                        self.buffer.set_transform_static(tf, 'bag')
             except ROSBagException:
                 print('Could not read')
 
@@ -186,10 +178,6 @@ class Reader:
                 yield topic_name
         return None
 
-    @staticmethod
-    def create_name_for_video(topic_name):
-        return topic_name.replace('/', '_')[1:]
-
     def calculate_fps(self, topic_name, save_interval):
         video_duration = 20
         return self.topics_info[topic_name].message_count / (video_duration * save_interval)
@@ -203,9 +191,9 @@ class Reader:
             self.data_availability["slam"] = True
         return self.data_availability
 
-    @staticmethod
-    def slots(msg):
-        return [getattr(msg, var) for var in msg.__slots__]
+    def get_matrix_from_lidar_to_static_frame(self, matrix_base_link_lidar, save_time):
+        matrix_icp = self.matrices_base_link_map[np.unique(np.searchsorted(self.saved_times_icp, save_time))[0]]
+        return matrix_icp @ matrix_base_link_lidar
 
     def get_matrix_from_lidar_to_base_link(self, topic_name):
         for msg_number, (topic, msg, time) in enumerate(self.bags[0].read_messages(topics=[topic_name])):
@@ -218,8 +206,23 @@ class Reader:
                 return numpify(transform_base_link_lidar.transform)
             except ExtrapolationException:
                 print(f"Transformation from lidar coordinate system to base_link was not found.")
-                return None
             except LookupException as e:
                 missing_frame = str(e).split()[0]
                 print(f"Frame {missing_frame} doesn't exist")
-                return None
+        return None
+
+    @staticmethod
+    def add_matrix_to_matrices_base_link_static_frame(rotation_matrix, translation,
+                                                      matrices_base_link_static_frame):
+        transform_matrix = np.eye(4)
+        transform_matrix[:3, :3] = rotation_matrix
+        transform_matrix[:3, 3] = translation
+        matrices_base_link_static_frame.append(transform_matrix)
+
+    @staticmethod
+    def create_name_for_video(topic_name):
+        return topic_name.replace('/', '_')[1:]
+
+    @staticmethod
+    def slots(msg):
+        return [getattr(msg, var) for var in msg.__slots__]
