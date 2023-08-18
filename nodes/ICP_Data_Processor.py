9bc85f3d0aec8b77af20227d5821a01bf5dd2870
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-08-18 15:00:31 +0200

    update:optimization, some now ifs.

--- nodes/ICP_Data_Processor.py
+++ nodes/ICP_Data_Processor.py
@@ -12,6 +12,9 @@ class ICPDataProcessor:
         self.first_transform_icp = None
         self.transformed_icp = None
         self.speeds = None
+        self.distances_icp = None
+        self.start_of_moving = None
+        self.end_of_moving = None
 
     def read_icp_topic(self):
         icp = []
@@ -47,7 +50,15 @@ class ICPDataProcessor:
         inv_matrix = np.linalg.inv(self.first_rotation_matrix_icp[:3, :3])
         coordinates = np.concatenate(icp, axis=1)
         self.transformed_icp = inv_matrix @ coordinates - np.expand_dims(inv_matrix @ coordinates[:, 0], axis=1)
-        return self.transformed_icp
+
+    def calculate_speeds_icp(self):
+        if self.transformed_icp is None:
+            return None
+        if self.speeds is None:
+            distances_one_period = np.abs(self.transformed_icp.T[1:] - self.transformed_icp.T[:-1])
+            times_one_period = self.times_icp[1:] - self.times_icp[:-1]
+            speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
+            self.speeds = np.linalg.norm(speeds_xyz, axis=1)
 
     def get_icp_topic(self):
         for topic_name, topic_info in self.bag.get_type_and_topic_info()[1].items():
@@ -58,35 +69,28 @@ class ICPDataProcessor:
     def get_distances_icp(self):
         if self.transformed_icp is None:
             return None
-        transpose_coordinates = self.transformed_icp.T
-        distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
-        distances_xyz = np.concatenate((np.zeros((1, 3)), np.cumsum(distances_one_period, axis=0)), axis=0)
-        distances_icp = np.linalg.norm(distances_xyz, axis=1)
-        return distances_icp
-
-    def get_speeds_icp(self):
-        if self.transformed_icp is None:
-            return None
-        distances_one_period = np.abs(self.transformed_icp.T[1:] - self.transformed_icp.T[:-1])
-        times_one_period = self.times_icp[1:] - self.times_icp[:-1]
-        speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
-        self.speeds = np.linalg.norm(speeds_xyz, axis=1)
-        return self.speeds
+        if self.distances_icp is None:
+            transpose_coordinates = self.transformed_icp.T
+            distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
+            distances_xyz = np.concatenate((np.zeros((1, 3)), np.cumsum(distances_one_period, axis=0)), axis=0)
+            self.distances_icp = np.linalg.norm(distances_xyz, axis=1)
+        return self.distances_icp
 
     def get_average_speed(self):
         return np.sum(self.speeds) / len(self.speeds) if self.speeds is not None else None
 
     def get_start_and_end_of_moving(self):
-        self.speeds = self.get_speeds_icp()
-        if self.speeds is None:
-            return None, None
-        moving = np.where(self.speeds > 0.2)[0]
-        if len(moving) == 0:
-            return None, None
-        saved_times = self.times_icp
-        start_of_moving = saved_times[moving[0]]
-        end_of_moving = saved_times[moving[-1] + 1] if len(moving) < len(saved_times) else saved_times[moving[-1]]
-        return start_of_moving, end_of_moving
+        if self.start_of_moving is None and self.end_of_moving is None:
+            self.calculate_speeds_icp()
+            if self.speeds is None:
+                return None, None
+            moving = np.where(self.speeds > 0.2)[0]
+            if len(moving) == 0:
+                return None, None
+            saved_times = self.times_icp
+            self.start_of_moving = saved_times[moving[0]]
+            self.end_of_moving = saved_times[moving[-1] + 1] if len(moving) < len(saved_times) else saved_times[moving[-1]]
+        return self.start_of_moving, self.end_of_moving
 
     def get_times_icp(self):
         return self.times_icp
--- nodes/ODOM_Data_Processor.py
+++ nodes/ODOM_Data_Processor.py
@@ -8,11 +8,14 @@ class ODOMDataProcessor:
     def __init__(self, bag):
         self.bag = bag
         self.times_odom = []
+        self.matrices_odom = []
         self.first_rotation_matrix_odom = None
         self.first_transform_odom = None
         self.transformed_odom = None
-        self.matrices_odom = []
         self.speeds = None
+        self.distances_odom = None
+        self.start_of_moving = None
+        self.end_of_moving = None
 
     def read_odom_topic(self):
         odom = []
@@ -33,6 +36,7 @@ class ODOMDataProcessor:
                 self.first_transform_odom = np.array([[position.x], [position.y], [position.z]])
             print(f"The Coordinates from frame 'base_link' to frame 'odom' are saved. Time: {save_time}")
             self.times_odom.append(save_time)
+        self.times_odom = np.array(self.times_odom)
         return odom
 
     def add_matrix_to_matrices_odom(self, rotation_matrix, translation):
@@ -47,7 +51,6 @@ class ODOMDataProcessor:
         inv_matrix = np.linalg.inv(self.first_rotation_matrix_odom[:3, :3])
         coordinates = np.concatenate(odom, axis=1)
         self.transformed_odom = inv_matrix @ coordinates - np.expand_dims(inv_matrix @ coordinates[:, 0], axis=1)
-        return self.transformed_odom
 
     def get_odom_topic(self):
         for topic_name, topic_info in self.bag.get_type_and_topic_info()[1].items():
@@ -58,31 +61,34 @@ class ODOMDataProcessor:
     def get_distances_odom(self):
         if self.transformed_odom is None:
             return None
-        transpose_coordinates = self.transformed_odom.T
-        distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
-        distances_xyz = np.concatenate((np.zeros((1, 3)), np.cumsum(distances_one_period, axis=0)), axis=0)
-        distances_odom = np.linalg.norm(distances_xyz, axis=1)
-        return distances_odom
+        if self.distances_odom is None:
+            transpose_coordinates = self.transformed_odom.T
+            distances_one_period = np.abs(transpose_coordinates[1:] - transpose_coordinates[:-1])
+            distances_xyz = np.concatenate((np.zeros((1, 3)), np.cumsum(distances_one_period, axis=0)), axis=0)
+            self.distances_odom = np.linalg.norm(distances_xyz, axis=1)
+        return self.distances_odom
 
     def get_start_and_end_of_moving(self):
-        self.speeds = self.get_speeds_odom()
-        if self.speeds is None:
-            return None, None
-        moving = np.where(self.speeds > 0.2)[0]
-        if len(moving) == 0:
-            return None, None
-        saved_times = self.times_odom
-        start_of_moving = saved_times[moving[0]]
-        end_of_moving = saved_times[moving[-1] + 1] if len(moving) < len(saved_times) else saved_times[moving[-1]]
-        return start_of_moving, end_of_moving
+        if self.start_of_moving is None and self.end_of_moving is None:
+            self.get_speeds_odom()
+            if self.speeds is None:
+                return None, None
+            moving = np.where(self.speeds > 0.2)[0]
+            if len(moving) == 0:
+                return None, None
+            saved_times = self.times_odom
+            self.start_of_moving = saved_times[moving[0]]
+            self.end_of_moving = saved_times[moving[-1] + 1] if len(moving) < len(saved_times) else saved_times[moving[-1]]
+        return self.start_of_moving, self.end_of_moving
 
     def get_speeds_odom(self):
         if self.transformed_odom is None:
             return None
-        distances_one_period = np.abs(self.transformed_odom.T[1:] - self.transformed_odom.T[:-1])
-        times_one_period = self.times_odom[1:] - self.times_odom[:-1]
-        speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
-        self.speeds = np.linalg.norm(speeds_xyz, axis=1)
+        if self.speeds is None:
+            distances_one_period = np.abs(self.transformed_odom.T[1:] - self.transformed_odom.T[:-1])
+            times_one_period = self.times_odom[1:] - self.times_odom[:-1]
+            speeds_xyz = distances_one_period / times_one_period.reshape(-1, 1)
+            self.speeds = np.linalg.norm(speeds_xyz, axis=1)
         return self.speeds
 
     def get_transformed_odom(self):
