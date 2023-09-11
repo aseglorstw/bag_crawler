73b411074d6bffa5379cbfda9d9773b2a6dd0788
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2023-09-11 14:16:50 +0200

    update:now I can remove bag files from .ignore.json file.

--- nodes/Bag_Ignore.py
+++ nodes/Bag_Ignore.py
@@ -7,8 +7,12 @@ def main(args):
     operation = args[1]
     path_to_bag_file = args[2]
     path_to_ignore_file = get_path_to_ignore_file(path_to_bag_file)
-    if operation == "-a":
+    if operation == "-add":
         add_bag_to_ignore_file(path_to_bag_file, path_to_ignore_file)
+    elif operation == "-rm":
+        remove_bag_from_ignore_file(path_to_bag_file, path_to_ignore_file)
+    else:
+        print("")
 
 
 def get_path_to_ignore_file(path_to_bag_file):
@@ -25,7 +29,7 @@ def add_bag_to_ignore_file(path_to_bag_file,  path_to_ignore_file):
     else:
         with open(path_to_ignore_file, "r", encoding="utf-8") as file:
             ignored_bag_files = json.load(file)
-            if path_to_bag_file in ignored_bag_files:
+            if bag_file in ignored_bag_files:
                 print(f"The bag file '{path_to_bag_file}' is already in '{path_to_ignore_file}'.")
                 return
             ignored_bag_files.append(bag_file)
@@ -37,11 +41,15 @@ def remove_bag_from_ignore_file(path_to_bag_file, path_to_ignore_file):
     if not os.path.exists(path_to_ignore_file):
         print(f"The file '{path_to_ignore_file}' doesn't exist.")
         return
+    _, bag_file = os.path.split(path_to_bag_file)
     with open(path_to_ignore_file, "r", encoding="utf-8") as file:
         ignored_bag_files = json.load(file)
-        if path_to_bag_file not in ignored_bag_files:
+        if bag_file not in ignored_bag_files:
             print(f"The file '{path_to_bag_file}' isn't in '{path_to_ignore_file}'.")
             return
+        ignored_bag_files.remove(bag_file)
+    with open(path_to_ignore_file, "w", encoding="utf-8") as file:
+        json.dump(ignored_bag_files, file, indent=4)
 
 
 if __name__ == '__main__':
