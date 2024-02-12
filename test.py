5f7082526a94ea32ab47fa7a5131df13a0d01563
Robert Bakhtigariev <robert.bakhtigariev@gmail.com>
2024-02-12 22:04:28 +0100

    update: new sif file.

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-path = "/home/robert/catkin_ws/src/bag_crawler/.bag_crawler_global_config.json"
-config = {"elements_of_control": {"/joy_local/cmd_vel": "robot_gamepad", "/nav/cmd_vel": "autonomous"},
-          "panorama topics": None}
-with open(path, "w", encoding="utf-8") as file:
-    json.dump(config, file, indent=4)
-
-
